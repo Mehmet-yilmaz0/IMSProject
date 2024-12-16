19ab6727015ea9b8cc7245f0fd581f571a156640
Mehmet <mhmt_ylmz_@outlook.com>
2024-12-16 23:26:32 +0300

    Model olusturma

---
+++ BYS.entities/model/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYS.entities.model
+{
+    public class Student
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentId { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string FullName { get; set; }
+        [Range(0, 2, ErrorMessage = "sayi aralik hatasi")]
+        public int OptionalCourse { get; set; } = 0;
+        [Range(0, 5, ErrorMessage = "sayi aralik hatasi")]
+        public int MandatoryCourse { get; set; } = 0;
+        public int AdvisorId { get; set; }
+    }
+}
